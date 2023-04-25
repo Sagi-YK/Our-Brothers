ef7773bbe1a21509f7f736c506a51af81bd0ae64
Sagi Yaakov <sagiyaakov2@gmail.com>
2023-04-25 20:23:08 +0300

    Creating the components folder

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Example from './components/Example';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Our Brothers!</Text>
+      <Example/>
       <StatusBar style="auto" />
     </View>
   );
