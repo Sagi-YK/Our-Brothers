fbe02ed4fe9c15a7c6e5c7a5fbf14822e368f8e7
Sagi Yaakov <sagiyaakov2@gmail.com>
2023-04-30 21:57:47 +0300

    Adding Bottom Tags Navigator

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Example from './components/Example';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Example/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          height: '10%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'center'
+        }
+      }}>
+        <Tab.Screen
+          name='Profile'
+          component={Example}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name='account' color={color} size={30} />)
+          }}
+        />
+        <Tab.Screen
+          name="Plus"
+          component={Example}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name='plus-circle' color={color} size={40} />),
+          }}
+        />
+        <Tab.Screen
+          name='Home'
+          component={Example}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name='home' color={color} size={30} />)
+          }}
+        />
+      </Tab.Navigator>
+      <StatusBar style={'dark'}/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
