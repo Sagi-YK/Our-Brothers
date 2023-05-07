c2232e26f55b1290a4af14092c553c0c1ad43c90
iyar levi <iyarlevi5@gmail.com>
2023-05-07 20:57:20 +0300

    Basic Profile Screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,14 +1,57 @@
 /**
- * this component will display an Our Brother message
+ * this component will display the profile page
  */
 
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 
-const ProfileScreen = () => {
+// const ProfileScreen = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Our Brothers</Text>
+//     </View>
+//   );
+// };
+const ProfileScreen = ({ navigation }) => {
+  const goToPage1 = () => {
+    navigation.navigate("Page1");
+  };
+
+  const goToPage2 = () => {
+    navigation.navigate("Page2");
+  };
+
+  const goToPage3 = () => {
+    navigation.navigate("Page3");
+  };
+
+  const goToPage4 = () => {
+    navigation.navigate("Page4");
+  };
+
+  // const goToPage5 = () => {
+  //   navigation.navigate("Page5");
+  // };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Our Brothers</Text>
+      <View style={styles.containerHead}>
+        <Text style={styles.headText}>שלום</Text>
+      </View>
+      <View style={styles.containerButtons}>
+        <TouchableOpacity style={styles.button} onPress={goToPage1}>
+          <Text style={styles.buttonText}>התחברות</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={goToPage2}>
+          <Text style={styles.buttonText}>המיזמים שלי</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={goToPage3}>
+          <Text style={styles.buttonText}>עדכון פרטים</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={goToPage4}>
+          <Text style={styles.buttonText}>הגדרות</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -16,12 +59,41 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
   },
-  text: {
-    fontSize: 50,
+  containerHead: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    marginTop: 60,
+  },
+  containerButtons: {
+    flex: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+  button: {
+    width: 200,
+    height: 60,
+    marginBottom: 30,
+    borderRadius: 10,
+    backgroundColor: "#007aff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  headText: {
+    color: "#000",
+    fontSize: 32,
+    fontWeight: "bold",
   },
 });
 
